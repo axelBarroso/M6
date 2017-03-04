797cfedd18bb1cf7ee2063752f40dd8ee5af8df5
sergi <sergiosanchoasensio@gmail.com>
2017-03-04 14:37:39 +0100

    Taking corners into account.

--- Code/Week1/apply_H.m
+++ Code/Week1/apply_H.m
@@ -1,4 +1,4 @@
-function [ outI ] = apply_H( I, H)
+function [ outI ] = apply_H(I, H)
 % apply_H function that applies transformation H to image I
 % returns image outI. 
 
@@ -14,10 +14,26 @@ if ((sizeH(1) ~= 3) || (sizeH(2) ~= 3))
 end
 
 
-xmin = 1;
-xmax = ncols;
-ymin = 1;
-ymax = nrows;
+% Find corners in homogeneous coord.
+c1 = [1 1 1]';
+c2 = [ncols 1 1]';
+c3 = [1 nrows 1]';
+c4 = [ncols nrows 1]';
+
+Hc1=H*c1;
+Hc2=H*c2;
+Hc3=H*c3;
+Hc4=H*c4;
+
+Hc1 = Hc1/Hc1(3);
+Hc2 = Hc2/Hc2(3);
+Hc3 = Hc3/Hc3(3);
+Hc4 = Hc4/Hc4(3);
+
+xmin = round(min([Hc1(1) Hc2(1) Hc3(1) Hc4(1)]));
+xmax = round(max([Hc1(1) Hc2(1) Hc3(1) Hc4(1)]));
+ymin = round(min([Hc1(2) Hc2(2) Hc3(2) Hc4(2)]));
+ymax = round(max([Hc1(2) Hc2(2) Hc3(2) Hc4(2)]));
 
 [X,Y] = meshgrid(xmin:xmax, ymin:ymax);
 Hncols = xmax - xmin + 1;
@@ -34,13 +50,11 @@ HZ = reshape(HiXYZs(3,:), Hnrows, Hncols);
 HX = HX ./ HZ;
 HY = HY ./ HZ;
 
-nchan= size(I,3);
-
 for c=1:nchan,
     outI(:,:,c) = interp2(double(I(:,:,c)), HX, HY, 'linear', 0);
 end
 
-figure; imshow(uint8(I)); title('original image');
-figure; imshow(uint8(outI)); title('transformed image');
+% figure; imshow(uint8(I)); title('original image');
+% figure; imshow(uint8(outI)); title('transformed image');
 
 end
