a744048dfee2c9031afb1fb07397ad3c0d77c0a0
sergi <sergiosanchoasensio@gmail.com>
2017-03-04 13:44:45 +0100

    SVD and reconstruction are implemented.

--- Code/Week1/lab1.m
+++ Code/Week1/lab1.m
@@ -18,9 +18,8 @@ I=imread('Data/0005_s.png'); % we have to be in the proper folder
 angle = 15
 theta = degtorad(angle);
 scale_factor = 0.5;
-translation = [100 100];
+translation = [100 75];
 H = [scale_factor*cos(theta) -scale_factor*sin(theta) translation(1); scale_factor*sin(theta)  scale_factor*cos(theta) translation(2) ; 0 0 1];
-
 
 I2 = apply_H(I, H);
 figure; imshow(I); figure; imshow(uint8(I2));
@@ -29,16 +28,35 @@ figure; imshow(I); figure; imshow(uint8(I2));
 %% 1.2. Affinities
 
 % ToDo: generate a matrix H which produces an affine transformation
+% Compute H a <- p. It maps an ideal point (x1,x2,0)^T to a finite point
+% (x1,x2,v1x1+v2x2)^T
 
 I2 = apply_H(I, H);
 figure; imshow(I); figure; imshow(uint8(I2));
 
+
 % ToDo: decompose the affinity in four transformations: two
 % rotations, a scale, and a translation
+[U,S,V] = svd(H(1:size(H,1)-1,1:size(H,2)-1)) %performs a singular value decomposition of matrix A, such that A = U*S*V'.
+l = [0,0,1];
+padding = [0,0]';
+rotation_1 = [U*V' padding; l];
+rotation_2 = [V' padding; l];
+scaling = [S padding; l];
+translation = [1 0 H(1,size(H,1)); 0 1 H(2,size(H,1)); l];
 
 
 % ToDo: verify that the product of the four previous transformations
 % produces the same matrix H as above
+
+% Compute A, following Lecture 1, Page 43 of 48
+A = rotation_1*rotation_2'*scaling*rotation_2;
+% Add the translation terms to H
+H_new = translation*A;
+I2 = apply_H(I, H_new);
+figure; imshow(uint8(I2));
+
+
 
 % ToDo: verify that the proper sequence of the four previous
 % transformations over the image I produces the same image I2 as before
