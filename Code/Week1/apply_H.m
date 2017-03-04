ac8543e0ffb1b906d4929c6bd880d0a3ddc6c621
SCLSCL <sergio.castro.latorre@gmail.com>
2017-03-04 11:18:41 +0100

    Add files via upload

--- Code/Week1/apply_H.m
+++ Code/Week1/apply_H.m
@@ -15,7 +15,7 @@ end
 xRange = [0, sizeI(2)-1];
 yRange = [0, sizeI(1)-1];
 
-box = [xRange(0); yRange(0); xRange(1); yRange(1)];
+box = [xRange(1); yRange(1); xRange(2); yRange(2)];
 
 % [x,y] = meshgrid(minx:maxx-1,miny:maxy-1);
 % 
@@ -30,8 +30,35 @@ box = [xRange(0); yRange(0); xRange(1); yRange(1)];
 % 
 % I2 = uint8(I2);
 
+[nrows, ncols, nchan] = size(I);
 
+xmin = 1;
+xmax = ncols;
+ymin = 1;
+ymax = nrows;
 
+[X,Y] = meshgrid(xmin:xmax, ymin:ymax);
+Hncols = xmax - xmin + 1;
+Hnrows = ymax - ymin + 1;
+Z = ones(Hnrows,Hncols);
 
+XYZs = [X(:) Y(:) Z(:)]';
+
+Hi = inv(H);
+HiXYZs = Hi * XYZs;
+HX = reshape(HiXYZs(1,:), Hnrows, Hncols);
+HY = reshape(HiXYZs(2,:), Hnrows, Hncols);
+HZ = reshape(HiXYZs(3,:), Hnrows, Hncols);
+HX = HX ./ HZ;
+HY = HY ./ HZ;
+
+nchan= size(I,3);
+
+for c=1:nchan,
+    outI(:,:,c) = interp2(double(I(:,:,c)), HX, HY, 'linear', 0);
 end
 
+figure; imshow(uint8(I)); title('original image');
+figure; imshow(uint8(outI)); title('transformed image');
+
+end
