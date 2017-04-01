2ebf7d40b043cc862effd95f88a27c405b121fad
SCLSCL <sergio.castro.latorre@gmail.com>
2017-04-01 12:37:38 +0200

    Add files via upload

--- Code/Week4/provided_files/triangulate.m
+++ Code/Week4/provided_files/triangulate.m
@@ -1,7 +1,7 @@
 function Xtrain = triangulate (x1, x2, P1, P2, imsize)
 h= [ 2/imsize(1) 0 -1
     0 2/imsize(2) -1
-    0 0 -1];
+    0 0 1];
 
 x11=euclid(h*homog(x1));
 x22=euclid(h*homog(x2));
@@ -14,15 +14,17 @@ Aprim= [A(:,1) A(:,2) A(:,3)];
 
 [U,D,VT] = svd(Aprim);
 UT=transpose(U);
-A4prim=UT*A(:,4);
+A4prim1=UT*A(:,4);
+A4prim=euclid(A4prim1);
 
 for i=1:size(D,2)
-    Y(i)= A4prim(i)/D(i,i);
+    Y(i,1)= (-A4prim(i))/D(i,i);
 end
 
 V=transpose(VT);
 Xtrain = V*Y;
 
+Xtrain=homog(Xtrain);
 
 
 end
